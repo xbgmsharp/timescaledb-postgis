c5673cb35ac9cfb82d2a64cca0a951d4caf9d416
xbgmsharp <xbgmsharp@gmail.com>
2023-07-12 14:38:46 +0200

    Update docker-test github workflow, wait for the DB to init, first launch. Update extensions tests scripts

--- tests/extensions_test/test_pgcrypto.sh
+++ tests/extensions_test/test_pgcrypto.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo "Test pgcrypto"
-psql -c "CREATE EXTENSION IF NOT EXISTS pgcrypto;"
+psql -c "CREATE EXTENSION pgcrypto;"
 psql -c "CREATE TABLE user_passwords (username varchar(100) PRIMARY KEY, crypttext text);"
 psql -c "INSERT INTO user_passwords (username, crypttext)
 VALUES ('user1', pgp_sym_encrypt('user1_password','<Password_Key>')),
